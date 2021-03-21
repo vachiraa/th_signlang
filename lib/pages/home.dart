e8fb8b73212d1916896ab1758fdd1d344db4bdb0
vachiraa <vachira.skp@gmail.com>
2021-03-21 16:48:19 +0700

    allow camery and gallery permission

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,7 +16,6 @@ class _HomePageState extends State<HomePage> {
   // File videoFile;
   final _picker = ImagePicker();
 
-
   Future videoMedia() async {
     final getVideo = await _picker.getVideo(source: ImageSource.camera);
     final File file = File(getVideo.path);
@@ -24,25 +23,32 @@ class _HomePageState extends State<HomePage> {
     if (file != null) {
       setState(() {
         // videoFile = file;
-        Navigator.push(context, MaterialPageRoute(builder: (context) => ProcessingPage(videoFile: file,)
-
-      )
-        );
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ProcessingPage(
+                      videoFile: file,
+                    )));
       });
     }
   }
 
   Future galleryMedia() async {
     final getVideo = await _picker.getVideo(source: ImageSource.gallery);
-    final File file = File(getVideo.path);
+    if (getVideo != null) {
+      final File file = File(getVideo.path);
 
-    if (file != null) {
-      setState(() {
-        // videoFile = file;
-        Navigator.push(context, MaterialPageRoute(
-            builder: (context) => ProcessingPage(videoFile: file,)
-        ));
-      });
+      if (file != null) {
+        setState(() {
+          // videoFile = file;
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ProcessingPage(
+                        videoFile: file,
+                      )));
+        });
+      }
     }
   }
 
@@ -56,63 +62,75 @@ class _HomePageState extends State<HomePage> {
           // decoration: BoxDecoration(gradient: LinearGradient(colors: [Color.fromRGBO(54, 54, 54, 100)],
           // begin: Alignment.topCenter,end: Alignment.bottomCenter)),
           child: Column(
-              children: <Widget>[
-
-                Container(
-                  child: Padding(
-                      padding: EdgeInsets.fromLTRB(10, 130, 10, 10),
+            children: <Widget>[
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(10, 130, 10, 10),
                   child: ClipRRect(
                     child: Image.asset(
                       'lib/assets/logo04.jpeg',
                       // width: 160.0,
                       // height: 160.0,
-                      width: MediaQuery.of(context).size.width *  0.35,
+                      width: MediaQuery.of(context).size.width * 0.35,
                     ),
                     borderRadius: BorderRadius.circular(26.0),
-                  ),),),
-                Container(
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 48),
-                    child: Text('''THAI SIGN LANGUAGE 
+                  ),
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 48),
+                  child: Text(
+                    '''THAI SIGN LANGUAGE 
        TRANSLATION''',
-                    style: TextStyle(color: Colors.white, fontSize: 20),),),),
-                Container(
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(0, 20, 0, 15),
-                    child: SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.07,
-                      width: MediaQuery.of(context).size.width *  0.40,
-                      child: RaisedButton(
-                        child: Text('Camera', style: TextStyle(fontSize: 24),),
-                      shape: StadiumBorder(),
-                      onPressed: () {videoMedia();},
-                      color: Theme.of(context).primaryColor,
-                      textColor: Colors.white,
-                  ),),),),
-                Container(
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(0, 10, 0, 20),
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 20, 0, 15),
                   child: SizedBox(
                     height: MediaQuery.of(context).size.height * 0.07,
-                    width: MediaQuery.of(context).size.width *  0.40,
+                    width: MediaQuery.of(context).size.width * 0.40,
+                    child: RaisedButton(
+                      child: Text(
+                        'Camera',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      shape: StadiumBorder(),
+                      onPressed: () {
+                        videoMedia();
+                      },
+                      color: Theme.of(context).primaryColor,
+                      textColor: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 10, 0, 20),
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.07,
+                    width: MediaQuery.of(context).size.width * 0.40,
                     child: RaisedButton(
                       child: Text('Gallery', style: TextStyle(fontSize: 24)),
                       shape: StadiumBorder(),
                       onPressed: () {
-                      galleryMedia();
-                    },
-                    color: Theme.of(context).primaryColor,
-                    textColor: Colors.white,
-                    padding: EdgeInsets.fromLTRB(35, 10, 35, 10),
-                  ),),),),
-              ],
-          ),
+                        galleryMedia();
+                      },
+                      color: Theme.of(context).primaryColor,
+                      textColor: Colors.white,
+                      padding: EdgeInsets.fromLTRB(35, 10, 35, 10),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
-
-
-
-
 }
